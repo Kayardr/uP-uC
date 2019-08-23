2f80d51620371dd9690036826cfdab40e743427f
kayardr <ricardo.castanon@uabc.edu.mx>
2019-08-22 20:36:21 -0700

    Tamagotchi, funcionando, remodelaciones

--- Taller/Taller_2/tamagotchi.c
+++ Taller/Taller_2/tamagotchi.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <time.h>
+#include <string.h>
 
 typedef struct pet_stats{
 	int current;
@@ -19,45 +22,109 @@ void delay(double delay_t){
 	while((clock() - begining) < delay_t);
 }
 
-void initialization(pet *m_pet){						//Stats base
-	m_pet->healt.current = 100;							//Salud inicial
-	m_pet->healt.alarm = 60;							//Nivel minimo para alarma
-	m_pet->healt.frecuency = 60;						//Tiempo con el que sieminuye salud
-	
-	//========== Alimentacion ============
-	m_pet->feeding.current = 100;						//Alimentación inicial
-	m_pet->feeding.alarm = 60;							//Alarma de alimento
-	m_pet->feeding.frecuency = 60;						//Tiempo de disminución
-	
-	//========== Entretenimiento =========
-	m_pet->entertain.current = 100;						//
-	m_pet->entertain.alarm = 60;						//
-	m_pet->entertain.frecuency = 60;	
-	
-	//========== Limpieza ================
-	m_pet->cleaning.current = 100;						//
-	m_pet->cleaning.alarm = 30;							//
-	m_pet->cleaning.frecuency = 30;						//
-	
+void initialization(stats *stat, int current, int alarm, int frecuency){
+	stat->current = current;
+	stat->alarm = alarm;
+	stat->frecuency = frecuency;
+}
+
+void dismis(stats *stat, int dismis, int frecuency){
+	if(stat->frecuency > 1)
+		stat->frecuency -= 1;
+	else if(stat->current >=0 ){
+		stat->current -= dismis;
+		if(stat->current < 0)
+			stat->current = 0;
+		stat->frecuency = frecuency;
+	}
+}
+
+void increase(stats *stat, int increase){
+	if(stat->current < 100){
+		stat->current += increase;
+		if(stat->current > 100)
+			stat->current = 100;
+	}
+}
+
+void interact(pet *m_pet, char c){
+	switch(c){
+		case 'm' :
+			increase(&m_pet->healt, 5);
+			break;
+		case 'f' : 
+			if(m_pet->healt.current > 60)
+				increase(&m_pet->feeding, 10);
+			break;
+		case 'p' :
+			if(m_pet->healt.current > 60 || m_pet->feeding.current >60) 
+				increase(&m_pet->entertain, 5);
+			break;
+		case 'b' : 
+			if(m_pet->healt.current > 60 || m_pet->feeding.current > 60 || 
+				m_pet->entertain.current > 50)
+				increase(&m_pet->cleaning, 100);
+			break;
+		default: break;
+	}
+}
+
+int alarm(stats *stat, int min){
+	if(stat->current <= min)
+		return 1;
+	return 0;
 }
 
 int main(void){
 	pet m_pet;
+	char command[20];
+	int dead = 0;
+	strcpy(command, "cls");
 	time_t last = time(NULL);
 	unsigned int s_elapsed = 0;
-	initialization(&m_pet);
-
+	initialization(&m_pet.healt, 1, 60, 20);
+	initialization(&m_pet.feeding, 1, 60, 60);
+	initialization(&m_pet.entertain, 100, 50, 60);
+	initialization(&m_pet.cleaning, 100, 30, 30);
+	char c;
 	while(1){
-		if((time(NULL)-last)>=1){
-			last = time(NULL);
-			printf("%d\n", ++s_elapsed);
-			printf("H = %i\t", m_pet.healt.current);
-			printf("F = %i\t", m_pet.feeding.current);
-			printf("E = %i\t", m_pet.entertain.current);
-			printf("C = %i\t\n", m_pet.cleaning.current);
-		}
+		if(dead!=1){
+			if(kbhit()){
+				c = getch();
+				interact(&m_pet, c);
+			}
 
-		
+			if((time(NULL)-last)>=1){
+				system(command);
+				last = time(NULL);
+				dismis(&m_pet.healt, 5, 60);
+				dismis(&m_pet.feeding, 3, 60);
+				dismis(&m_pet.entertain, 5, 60);
+				dismis(&m_pet.cleaning, 5, 30);
+				if(m_pet.healt.current == 0 || m_pet.feeding.current == 0)
+					dead = 1;
+
+				printf("%d\n", ++s_elapsed);
+				printf("H = %i\t", m_pet.healt.current);
+				printf("F = %i\t", m_pet.feeding.current);
+				printf("E = %i\t", m_pet.entertain.current);
+				printf("C = %i\t\n", m_pet.cleaning.current);
+
+				if(alarm(&m_pet.healt, 60) == 1)
+					printf("HELP MEEEE!\n");
+				else if(alarm(&m_pet.feeding, 60) == 1)
+					printf("IM HUNGRY\n");
+				else if(alarm(&m_pet.entertain, 50) == 1)
+					printf("IM BORED\n");
+				else if(alarm(&m_pet.cleaning, 30) == 1)
+					printf("IM DIRTY\n");
+			}	
+		}
+		else{
+			system(command);
+			printf("YOUR PET HAS DIED\n" );
+			printf("Elapsed time (seconds): %i\n", s_elapsed);
+		}
 		
 		delay(200);
 	}
