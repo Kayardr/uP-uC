23dd1383ea936078b69c1225d18b35892182c204
Ricardo <ricardo.castanon@uabc.edu.mx>
2019-08-25 22:17:47 -0700

    funciones en ASM

---
+++ Laboratorio/Practica-2/ifa.asm
@@ -0,0 +1,35 @@
+.model tiny
+	locals
+.data
+
+.code
+	org 100h
+
+;====================================================
+;	Headers
+;====================================================
+	public _getchar
+	public _putchar
+
+;====================================================
+;	Funciones
+;====================================================
+	_getchar proc
+		mov ah, 08h
+		int 21h
+		mov ah, 0
+		pop bp
+		ret
+	_getchar endp
+
+
+	_putchar proc
+		push bp
+		mov bp, sp
+		mov dl, [bp+4]
+		mov ah, 02h
+		int 21h
+		pop bp
+		ret
+	_putchar endp
+END
--- Laboratorio/Practica-2/pra2.c
+++ Laboratorio/Practica-2/pra2.c
@@ -1,40 +1,42 @@
-void putchar( char dato );
+extern void putchar( char dato );
 void puts( char *str );
-char getch( void );
-void printdec(unsigned char dato );
+extern char getchar( void );
+void printdec(unsigned char dato);
+
 char msg[]="Hola UABC\r\n";
 unsigned char i=0;
+
 int main ( void ){
-while(1){
-printdec(i++);
-puts( msg );
-getch();
+	while(1){
+		printdec(i++);
+		puts( msg );
+		getchar();
+	}
+	return 0;
 }
-return 0;
+
+void puts ( char *str ){
+	while( *str )
+	putchar( *str++ );
 }
-void puts ( char *str )
-{
-while( *str )
-putchar( *str++ );
+/*
+void putchar ( char dato ){
+	asm mov dl,dato
+	asm mov ah,2
+	asm int 21h
 }
-void putchar ( char dato )
-{
-asm mov dl,dato
-asm mov ah,2
-asm int 21h
+
+char getch( void ){
+	char dato;
+	asm mov ah,8
+	asm int 21h
+	asm mov dato,al
+	return dato;
 }
-char getch( void )
-{
-char dato;
-asm mov ah,8
-asm int 21h
-asm mov dato,al
-return dato;
-}
-void printdec ( unsigned char dato )
-{
-putchar( dato/100 + 0x30 );
-dato%=100;
-putchar( dato/10 + 0x30 );
-putchar( dato%10 + 0x30 );
+*/
+void printdec ( unsigned char dato ){
+	putchar( dato/100 + 0x30 );
+	dato%=100;
+	putchar( dato/10 + 0x30 );
+	putchar( dato%10 + 0x30 );
 }
