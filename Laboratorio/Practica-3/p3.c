6894a0455e31249a4132d428847f62349d3eb3fc
kayardr <ricardo.castanon@uabc.edu.mx>
2019-09-21 10:40:32 -0700

    Terminar P3

--- Laboratorio/Practica-3/p3.c
+++ Laboratorio/Practica-3/p3.c
@@ -15,12 +15,12 @@ void main(){
 	unsigned char a;
 	unsigned char b;
 	a = getchar();
-	poke(200, 200, a);
-	b = peek(200, 200);
+	poke(0, 0x2200, a);
+	b = peek(0, 0x2200);
 	putchar(b);
 	getchar();
-	dataBusTest(0x200, 0x200);
-	addressBusTest(0x200, 0x200, 0x7FF);
+	dataBusTest(0, 0x2200);
+	addressBusTest(0, 0x2200, 0x7FFF);
 	getchar();
 }
 
@@ -36,6 +36,12 @@ void dataBusTest(unsigned int segment, unsigned int offset){
 	for(pattern = 1; pattern != 0; pattern <<=1){		/*El patron recorre cada line bite por bite*/
 		poke(segment, offset, pattern);					/*Se introduce el patro establecido en la direccion indicada*/
 		if(peek(segment, offset) != pattern){			/*Comprueba si los datos en la seccion de memoria no coinciden con el patron*/
+			
+			puts("\r\n");
+			printBin(peek(segment, offset));
+			puts("\r\n");
+			printBin(pattern);
+			puts("\r\n");
 			puts("Eror en ducto D: ");					/*De no coincidir indica un erro en el ducto de datos*/
 			putchar((char)i+'0');						/* que es indicado por la cantidad de bits recorridos*/
 			puts("\r\n");		
@@ -49,8 +55,8 @@ void addressBusTest(unsigned int segment, unsigned int offset, unsigned int nBit
 	unsigned int addresMask = nBits/sizeof(unsigned int)-1;			
 	unsigned int offsetb;
 	unsigned int testOffset;
-	unsigned char pattern = 0xAA;
-	unsigned char antipattern = 0x55;
+	unsigned char pattern = 0x2A;
+	unsigned char antipattern = 0x15;
 	unsigned int bad_pins = 0;
 	for(offsetb = 1 ; (offsetb & addresMask)!= 0; offsetb<<=1)
 		poke(segment, offset+offsetb, pattern);
@@ -69,7 +75,7 @@ void addressBusTest(unsigned int segment, unsigned int offset, unsigned int nBit
 			bad_pins = bad_pins | offsetb;		
 		}
 		for(offsetb = 1; (offsetb & addresMask) != 0; offsetb <<= 1){
-			if((peek(segment, offset+offsetb)) && (offsetb != testOffset))
+			if((peek(segment, offset+offsetb) != pattern) && (offsetb != testOffset))
 				bad_pins = bad_pins | offsetb;
 		}
 		poke(segment, offset+offsetb, pattern);
